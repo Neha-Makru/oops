ceced9b81e1f77a4c8a534397980cf7a78bd763d
Neha Makru <137904719+Neha-Makru@users.noreply.github.com>
2024-04-30 21:23:46 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -1,1 +1,25 @@
+//overloading function in oops 
 
+#include <iostream>
+using namespace std; 
+
+float area(float, float);
+float area(float); 
+
+int main(){
+  int r, b, h; 
+  cout<<"Enter the radius of the circle here:"<<endl; 
+  cin>>r; 
+  cout<<"Enter the base and the height of the traingle here: "<<endl; 
+  cin>>b>>h; 
+  cout<<"Area of the circle: "<<area(r)<<endl; 
+  cout<<"Area of the traingle: "<<area(b,h)<<endl; 
+  return 0;
+}
+
+float area(float r){
+  return (3.14*r*r);
+}
+float area(float b, float h){
+  return (0.5*b*h); 
+}
